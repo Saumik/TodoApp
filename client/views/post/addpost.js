fd667ef8c7a869227abc098d7057adb9fc914f82
Saumik <saumik@Saumiks-MacBook-Pro.local>
2016-03-14 02:25:54 +0530

    making it better

--- client/views/post/addpost.js
+++ client/views/post/addpost.js
@@ -1,9 +1,15 @@
 Template.addPost.events({
 
-'keypress input.addPost': function (evt, template) {
+'keypress textarea.addPost': function (evt, template) {
 	var addpost = template.find(".addPost").value;
     if (evt.which === 13 && addpost != "" && Meteor.user() != null) {
     	Meteor.call('sendlog', addpost);
     }
   }
 });
+
+Template.addPost.rendered = function() {
+  tinymce.init({
+    selector: 'textarea'
+  });
+};
