5686fce5615beceb5f933fcf89ab93b02f61307e
Saumik <saumik@Saumiks-MacBook-Pro.local>
2016-03-14 21:16:46 +0530

    app

--- client/views/post/addpost.js
+++ client/views/post/addpost.js
@@ -1,15 +1,25 @@
 Template.addPost.events({
 
-'keypress textarea.addPost': function (evt, template) {
-	var addpost = template.find(".addPost").value;
-    if (evt.which === 13 && addpost != "" && Meteor.user() != null) {
-    	Meteor.call('sendlog', addpost);
-    }
-  }
+'click .confirm': function(event){
+event.preventDefault();
+var addPost = tinyMCE.activeEditor.getContent();
+Meteor.call('sendlog', addPost);
+}
 });
 
 Template.addPost.rendered = function() {
   tinymce.init({
-    selector: 'textarea'
-  });
+  selector: 'textarea',
+  height: 500,
+  plugins: [
+    'advlist autolink lists link image charmap print preview anchor',
+    'searchreplace visualblocks code fullscreen',
+    'insertdatetime media table contextmenu paste code'
+  ],
+  toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+  content_css: [
+    '//fast.fonts.net/cssapi/e6dc9b99-64fe-4292-ad98-6974f93cd2a2.css',
+    '//www.tinymce.com/css/codepen.min.css'
+  ]
+});
 };
